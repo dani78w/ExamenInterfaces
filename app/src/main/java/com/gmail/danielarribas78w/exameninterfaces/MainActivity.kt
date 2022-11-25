c2038d01f17d4c3f33870961d9c3228705eceea4
Daniel Arribas <danielarribas78w@gmail.com>
2022-11-25 12:07:26 +0100

    Examen completo

---
+++ app/src/main/java/com/gmail/danielarribas78w/exameninterfaces/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.danielarribas78w.exameninterfaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
